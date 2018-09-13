84894d645adb06813f3080d7b3e1767f76c585c7
Andrew Rombach <andrew.rombach@uk.ibm.com>
2018-09-13 13:32:38 +0100

    Improved messages in test script

--- test/csa-testall.sh
+++ test/csa-testall.sh
@@ -16,11 +16,14 @@ else
  url=$3
 fi
 
-epoch=`date +%%Y%m%d%H%M%S`
+epoch=`date +%Y%m%d%H%M%S`
+echo 'running managedb tests'
 ./csa-test-managedb-unauth.sh $user $password $url > ./resultlog/csa-managedb-unauth.log
 ./csa-test-managedb-pass.sh $user $password $url > ./resultlog/csa-managedb-pass.log
+echo 'running migrate-view tests'
 ./csa-test-migrate-view-pass.sh $user $password $url > ./resultlog/csa-migrate-view-pass.log
 ./csa-test-migrate-view-unauth.sh $user $password $url > ./resultlog/csa-migrate-view-unauth.log
+echo 'running migrate-index tests'
 ./csa-test-migrate-index-pass.sh $user $password $url > ./resultlog/csa-migrate-index-pass.log
 ./csa-test-migrate-index-unauth.sh $user $password $url > ./resultlog/csa-migrate-index-unauth.log
 echo 'sleeping 60 seconds and then collecting logs in case problems with jobs'
